7ea51af67692f2f47681a3873002cb9c0d3d8006
26077914 <26077914@UNA009-L103-006.serlab.intra>
2018-10-23 21:32:39 -0300

    Java Web

--- src/java/br/com/exemplo/OlaMundo.java
+++ src/java/br/com/exemplo/OlaMundo.java
@@ -5,7 +5,23 @@
  */
 package br.com.exemplo;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(name="ola",urlPatterns = {"/oi"})
 public class OlaMundo extends HttpServlet {
+    protected void service(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException{
+        PrintWriter out = response.getWriter(); 
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Ol&aacute; mundo!");
+        out.println("</body>");
+        out.println("</html>");
+    }
     
 }
